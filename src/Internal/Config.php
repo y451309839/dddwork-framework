610a387eb8984605fed078556cc450a143fbff14
qin.yi <qin.yi@facegood.cc>
2024-11-12 10:01:22 +0800

    fix: Config

--- src/Internal/Config.php
+++ src/Internal/Config.php
@@ -2,6 +2,10 @@
 
 namespace Huizi\Dddwork\Internal;
 
+use FilesystemIterator;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+
 class Config
 {
 
@@ -11,8 +15,25 @@ class Config
 
     protected static $loaded = false;
 
-    public static function load($configPath)
+    public static function load($configPath, $exclude = [])
     {
 
+    }
+
+    public static function loadFromDir($configPath, $exclude = [])
+    {
+        $allConfig = [];
+        $dirIterator = new RecursiveDirectoryIterator($configPath, FilesystemIterator::FOLLOW_SYMLINKS);
+        $iterator = new RecursiveIteratorIterator($dirIterator);
+        foreach ($iterator as $file) {
+            if (is_dir($file) || $file->getExtension() != 'php' || in_array($file->getBaseName('.php'), $exclude)) {
+                continue;
+            }
+            $appConfigFile = $file->getPath() . '/app.php';
+            if (!is_file($appConfigFile)) {
+                continue;
+            }
+        }
+        return $allConfig;
     }
 }
